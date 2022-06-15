2541a0630329ef3c60ba7ab2ae0bd4c517273385
EvgeniyNakorenko <nakorenko_zheka@mail.ru>
2022-06-15 11:54:47 +0300

    added edits to 339b

--- task339b/src/339b.kt
+++ task339b/src/339b.kt
@@ -2,11 +2,10 @@ fun main(){
     val nm = readLine()!!.split(" ").map { it.toLong() }
     val ai = readLine()!!.split(" ").map { it.toLong() }
     var ans:Long=-1
-
     for (i in 0 until nm[1]){
-        if (i in 0 until 1) ans += ai[0]
-        else if (ai[i.toInt()]<ai[(i-1).toInt()]) ans += nm[0] -ai[(i-1).toInt()] +ai[i.toInt()]
-        else ans += ai[i.toInt()]-ai[(i-1).toInt()]
+        ans += if (i in 0 until 1) ai[0]
+        else if (ai[i.toInt()]<ai[(i-1).toInt()]) nm[0] -ai[(i-1).toInt()] +ai[i.toInt()]
+        else ai[i.toInt()]-ai[(i-1).toInt()]
     }
     print(ans)
 }
